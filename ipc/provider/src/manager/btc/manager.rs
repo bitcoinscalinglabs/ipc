ab4cde4e3363cc9b076170fea87293add03b50b3
Orestis Alpos <oralpos@gmail.com>
2025-03-01 12:38:21 +0200

    temporary fix for top-down nonce not starting at 0. Revert this when we change it on bitcoin-ipc.

--- ipc/provider/src/manager/btc/manager.rs
+++ ipc/provider/src/manager/btc/manager.rs
@@ -1020,6 +1020,10 @@ impl TopDownFinalityQuery for BtcSubnetManager {
                 .get("nonce")
                 .and_then(Value::as_u64)
                 .ok_or_else(|| anyhow!("Field nonce not found in result"))?;
+            // TODO(Orestis): This should be removed
+            if nonce == 0 {
+                return Err(anyhow!("Nonce is 0"));
+            }
 
             let envelope = IpcEnvelope {
                 kind,
@@ -1031,8 +1035,10 @@ impl TopDownFinalityQuery for BtcSubnetManager {
                     &SubnetID::new_root(subnet_id.root_id()),
                     &Address::new_delegated(BTC_NAMESPACE, &vec![0; 20])?,
                 )?,
+                // from: IPCAddress::new(&subnet_id, &target_address)?,
                 message: vec![],
-                nonce,
+                // TODO(Orestis): This should be removed
+                nonce: nonce - 1,
             };
             messages.push(envelope);
         }
