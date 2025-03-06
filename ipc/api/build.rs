3fc3115de0e3e27a06d3876c6574da5b787f634d
themicp <themicp@gmail.com>
2025-03-06 00:20:29 +0000

    Update ipc-api build script to build contracts

--- ipc/api/build.rs
+++ ipc/api/build.rs
@@ -3,21 +3,20 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    // Cargo sets the CARGO_MANIFEST_DIR env var to the *crate root* (where your Cargo.toml is).
-    // In your case, that's likely "ipc/api/".
-    // We want to run "make" in the parent directory, i.e. "ipc/".
+    let crate_dir =
+        env::var("CARGO_MANIFEST_DIR").expect("Missing CARGO_MANIFEST_DIR environment variable");
 
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("Missing CARGO_MANIFEST_DIR");
-    let parent_dir = Path::new(&crate_dir)
-        .parent() // go up one level
-        .expect("No parent directory found for crate directory");
+    let contracts_dir = Path::new(&crate_dir)
+        .join("../..") // go up two levels from "ipc/api" to "project_root"
+        .join("contracts");
 
     let status = Command::new("make")
-        .current_dir(parent_dir) // run "make" in "ipc/" instead of "ipc/api/"
+        .arg("gen")
+        .current_dir(&contracts_dir)
         .status()
-        .expect("Failed to run make in the parent folder");
+        .expect("Failed to run `make gen` in `contracts` directory");
 
     if !status.success() {
-        panic!("'make' command failed to run successfully in the 'ipc' folder");
+        panic!("`make gen` failed in `contracts` directory");
     }
 }
