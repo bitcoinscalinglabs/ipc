03c3a02d3467bdd8835b1d5aa70f2ace39630ce8
Orestis Alpos <oralpos@gmail.com>
2025-05-30 00:01:13 +0200

    a formatting change

--- fendermint/vm/interpreter/src/chain.rs
+++ fendermint/vm/interpreter/src/chain.rs
@@ -357,7 +357,7 @@ where
                         .context("failed to fetch validator changes")?;
 
                     println!(
-                        "validator_changes_from {} to {}: {:#?}",
+                        "validator_changes_from {} to {}: {:?}",
                         execution_fr, execution_to, validator_changes
                     );
 
