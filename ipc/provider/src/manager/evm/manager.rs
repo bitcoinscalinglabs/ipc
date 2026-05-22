9546cd973498b07fe26e674bc546875e83f13b87
Orestis Alpos <oralpos@gmail.com>
2026-05-22 12:08:52 +0300

    pass RESOLVER_EXTERNAL_ADDRESSES env var to docker containers

--- ipc/provider/src/manager/evm/manager.rs
+++ ipc/provider/src/manager/evm/manager.rs
@@ -8,10 +8,10 @@ use std::time::Duration;
 
 use ethers_contract::{ContractError, EthLogDecode, LogMeta};
 use ipc_actors_abis::{
-    checkpointing_facet, gateway_getter_facet, gateway_manager_facet,
-    gateway_messenger_facet, lib_gateway, lib_quorum, lib_staking_change_log,
-    register_subnet_facet, subnet_actor_activity_facet, subnet_actor_checkpointing_facet,
-    subnet_actor_getter_facet, subnet_actor_manager_facet, subnet_actor_reward_facet,
+    checkpointing_facet, gateway_getter_facet, gateway_manager_facet, gateway_messenger_facet,
+    lib_gateway, lib_quorum, lib_staking_change_log, register_subnet_facet,
+    subnet_actor_activity_facet, subnet_actor_checkpointing_facet, subnet_actor_getter_facet,
+    subnet_actor_manager_facet, subnet_actor_reward_facet,
 };
 use ipc_api::checkpoint::{BitcoinCheckpointSignatureQuorum, BottomUpCheckpointBundle};
 use ipc_api::evm::{fil_to_eth_amount, payload_to_evm_address, subnet_id_to_evm_addresses};
